e4997581db9518bc5fe361e1a2cac5a55add1bef
Andrew Hickey <andrewhickey@geniesolutions.com.au>
2018-06-25 19:09:41 +1000

    DAN-0001  Refactor exits, annd placeholder cmd loop, get player name

--- danventure.py
+++ danventure.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 
 logger = logging.getLogger()
 # logger.setLevel(logging.DEBUG)
@@ -104,6 +105,27 @@ class Screen:
         bg = ScrBNone
 
 
+class Directions:
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    UP = 4
+    DOWN = 5
+
+    NUM_DIRS = 6
+
+
+directions = [
+    {"name": "North", "abbrev": "N"},
+    {"name": "East", "abbrev": "E"},
+    {"name": "South", "abbrev": "S"},
+    {"name": "West", "abbrev": "W"},
+    {"name": "Up", "abbrev": "U"},
+    {"name": "Down", "abbrev": "D"}
+]
+
+
 class RoomTypes:
     DEV = 0
     GRASS = 1
@@ -159,13 +181,13 @@ def make_rooms():
     new_rooms = [
         {"id": 0, "name": "Dan's Dev Room", "type": RoomTypes.DEV,
          "desc": "This is a temporary dev room.  This is a plain white space for creating new stuff.",
-         "exitN": NOWHERE, "exitE": NOWHERE, "exitS": NOWHERE, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": 1,
+         "exits": [NOWHERE, NOWHERE, NOWHERE, NOWHERE, NOWHERE, 1],
          "special": NONE, "looks": []},
 
         {"id": 1, "name": "The South Garden", "type": RoomTypes.GRASS,
          "desc": "This is a beautiful and lush garden.  The grass is green and there are flowers and trees all " +
                  "around the place.",
-         "exitN": 2, "exitE": NOWHERE, "exitS": NOWHERE, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": NOWHERE,
+         "exits": [2, NOWHERE, NOWHERE, NOWHERE, NOWHERE, NOWHERE],
          "special": NONE, "looks": [
             {"name": "grass", "desc": "The grass is very lush and a health green."},
             {"name": "trees", "desc": "Tall, medium and short, the trees are lush and healthy."},
@@ -174,7 +196,7 @@ def make_rooms():
         {"id": 2, "name": "Outside the House", "type": RoomTypes.TRACK,
          "desc": "A small garden outside the front entrance.  Small garden beds full of flowers line the grass " +
                  "beside the path.",
-         "exitN": 3, "exitE": 4, "exitS": 1, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": NOWHERE,
+         "exits": [3, 4, 1, NOWHERE, NOWHERE, NOWHERE],
          "special": NONE, "looks": [
             {"name": "grass", "desc": "The grass is very lush and a health green."},
             {"name": "flowers", "desc": "Small white and yellow flowers are in the garden beds."},
@@ -183,7 +205,7 @@ def make_rooms():
         {"id": 3, "name": "The north garden", "type": RoomTypes.GRASS,
          "desc": "This is a lush and beautiful garden.  The grass is green and there are flowers and trees all " +
                  "around the place.",
-         "exitN": NOWHERE, "exitE": NOWHERE, "exitS": 1, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": NOWHERE,
+         "exits": [NOWHERE, NOWHERE, 1, NOWHERE, NOWHERE, NOWHERE],
          "special": NONE, "looks": [
             {"name": "grass", "desc": "The grass is very lush and a health green."},
             {"name": "trees", "desc": "Tall, medium and short, the trees are lush and healthy."},
@@ -192,48 +214,48 @@ def make_rooms():
         {"id": 4, "name": "Inside the Entrance Hall", "type": RoomTypes.INSIDE,
          "desc": "A small square room acts as an entrance to a dark hallway.  There is a small painting on the wall " +
                  "and the floor is covered by a carpet.",
-         "exitN": NOWHERE, "exitE": 5, "exitS": NOWHERE, "exitW": 2, "exitU": NOWHERE, "exitD": NOWHERE,
-         "special": NOTHING, "looks": [
+         "exits": [NOWHERE, 5, NOWHERE, 2, NOWHERE, NOWHERE],
+         "special": NONE, "looks": [
             {"name": "painting", "desc": "A small painting of a bowl of fruit."},
             {"name": "carpet", "desc": "The carpet is made of soft green wool."}]},
 
         {"id": 5, "name": "A Dark Hallway", "type": RoomTypes.INSIDE,
          "desc": "This hallway is long and quite dark.  The floor feels soft and deadens the sound a little.  " +
                  "Empty candle holder can just be seen on the walls.",
-         "exitN": NOWHERE, "exitE": 6, "exitS": NOWHERE, "exitW": 4, "exitU": NOWHERE, "exitD": NOWHERE,
+         "exits": [NOWHERE, 6, NOWHERE, 4, NOWHERE, NOWHERE],
          "special": NONE, "looks": []},
 
         {"id": 6, "name": "A Nexus in the Hallway", "type": RoomTypes.INSIDE,
          "desc": "The hallway comes to an end and rooms branch off it.  Food smells drift in from the" +
                  "north and there is a small room to the south.  The soft carpet is ragged here.",
-         "exitN": 7, "exitE": 9, "exitS": 8, "exitW": 5, "exitU": NOWHERE, "exitD": NOWHERE,
-         "special": NOTHING, "looks": []},
+         "exits": [7, 9, 8, 5, NOWHERE, NOWHERE],
+         "special": NONE, "looks": []},
 
         {"id": 7, "name": "The Kitchen", "type": RoomTypes.INSIDE,
          "desc": "A large wooden table is in the center of the kitchen.  Small cupboards line the walls.",
-         "exitN": NOWHERE, "exitE": NOWHERE, "exitS": 6, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": NOWHERE,
-         "special": NOTHING, "looks": []},
+         "exits": [NOWHERE, NOWHERE, 6, NOWHERE, NOWHERE, NOWHERE],
+         "special": NONE, "looks": []},
 
         {"id": 8, "name": "A Small Bedroom", "type": RoomTypes.INSIDE,
          "desc": "This room is rather small, but the floor is soft and the walls are brightly coloured.",
-         "exitN": 6, "exitE": NOWHERE, "exitS": NOWHERE, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": NOWHERE,
-         "special": NOTHING, "looks": []},
+         "exits": [6, NOWHERE, NOWHERE, NOWHERE, NOWHERE, NOWHERE],
+         "special": NONE, "looks": []},
 
         {"id": 9, "name": "The Back Room", "type": RoomTypes.INSIDE,
          "desc": "This large room has large windows and a set or stairs going up to the next floor.  There is also " +
                  "a large wooden door that has been wedged open.",
-         "exitN": NOWHERE, "exitE": 11, "exitS": NOWHERE, "exitW": 6, "exitU": 10, "exitD": NOWHERE,
-         "special": NOTHING, "looks": []},
+         "exits": [NOWHERE, 11, NOWHERE, 6, 10, NOWHERE],
+         "special": NONE, "looks": []},
 
         {"id": 10, "name": "The Attic", "type": RoomTypes.INSIDE,
          "desc": "This dark and dusty room has a lot of junk scattered around the place.  The is a small window " +
                  "looking out over the city.",
-         "exitN": NOWHERE, "exitE": NOWHERE, "exitS": NOWHERE, "exitW": NOWHERE, "exitU": NOWHERE, "exitD": 9,
-         "special": NOTHING, "looks": []},
+         "exits": [NOWHERE, NOWHERE, NOWHERE, NOWHERE, NOWHERE, 9],
+         "special": NONE, "looks": []},
 
         {"id": 11, "name": "A Small Alleyway", "type": RoomTypes.CITY,
          "desc": "A Small Alleyway",
-         "exitN": NOWHERE, "exitE": NOWHERE, "exitS": NOWHERE, "exitW": 9, "exitU": NOWHERE, "exitD": NOWHERE,
+         "exits": [NOWHERE, NOWHERE, NOWHERE, 9, NOWHERE, NOWHERE],
          "special": "City_Bell", "looks": [
             {"name": "dirt", "desc": "small piles of dirt and rubbish line the alleyway"}]}
     ]
@@ -256,7 +278,34 @@ def find_room(room_id, rooms):
     return rm
 
 
-def print_room(room_id, rooms):
+def print_exit_detail(dir, room, rooms):
+    """
+    :param dir:
+    :param room:
+    :param rooms:
+    :return:
+    """
+    global directions
+    if (dir < Directions.NORTH) or (dir >= Directions.NUM_DIRS):
+        # bad direction
+        return
+    elif len(room) == 0:
+        # bad room
+        return
+    elif len(room["exits"]) < Directions.NUM_DIRS:
+        # room has bad exits
+        return
+
+    exts = room["exits"]
+    ext = exts[dir]
+    if ext != NOWHERE:
+        print('\t{yel}{dname:5s}: [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
+              cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=ext,
+              exname=find_room(ext, rooms)["name"], dname=directions[dir]["name"]))
+    return
+
+
+def print_room_details(room_id, rooms):
     global max_room_id
     if (room_id > max_room_id) or (room_id < 0):
         return
@@ -271,30 +320,12 @@ def print_room(room_id, rooms):
             print('{yel}Description:{norm}'.format(yel=Screen.fg.YELLOW, norm=Screen.fg.NRM))
             print('{cyan}{rdesc}{norm}'.format(norm=Screen.fg.NRM, cyan=Screen.fg.CYAN, rdesc=rm["desc"]))
             print('{yel}Exits:{norm}'.format(yel=Screen.fg.YELLOW, norm=Screen.fg.NRM))
-            if rm["exitN"] != NOWHERE:
-                print('\t{yel}North: [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
-                      cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=rm["exitN"],
-                      exname=find_room(rm["exitN"], rooms)["name"]))
-            if rm["exitE"] != NOWHERE:
-                print('\t{yel}East : [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
-                      cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=rm["exitE"],
-                      exname=find_room(rm["exitE"], rooms)["name"]))
-            if rm["exitS"] != NOWHERE:
-                print('\t{yel}South: [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
-                      cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=rm["exitS"],
-                      exname=find_room(rm["exitS"], rooms)["name"]))
-            if rm["exitW"] != NOWHERE:
-                print('\t{yel}West : [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
-                      cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=rm["exitW"],
-                      exname=find_room(rm["exitW"], rooms)["name"]))
-            if rm["exitU"] != NOWHERE:
-                print('\t{yel}Up   : [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
-                      cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=rm["exitU"],
-                      exname=find_room(rm["exitU"], rooms)["name"]))
-            if rm["exitD"] != NOWHERE:
-                print('\t{yel}Down : [{cyan}{exnum:3d}:{exname}{yel}]{norm}'.format(yel=Screen.fg.YELLOW,
-                      cyan=Screen.fg.CYAN, norm=Screen.fg.NRM, exnum=rm["exitD"],
-                      exname=find_room(rm["exitD"], rooms)["name"]))
+            print_exit_detail(Directions.NORTH, rm, rooms)
+            print_exit_detail(Directions.EAST, rm, rooms)
+            print_exit_detail(Directions.SOUTH, rm, rooms)
+            print_exit_detail(Directions.WEST, rm, rooms)
+            print_exit_detail(Directions.UP, rm, rooms)
+            print_exit_detail(Directions.DOWN, rm, rooms)
             if len(rm["looks"]) >= 1:
                 print('{yel}Extras:{norm}'.format(yel=Screen.fg.YELLOW, norm=Screen.fg.NRM))
                 for lk in rm["looks"]:
@@ -316,18 +347,62 @@ def test_all_rooms(rooms):
     # print("\033[30;100m*\033[97;101m*\033[97;102m*\033[97;103m*\033[97;104m*\033[97;105m*\033[97;106m*\033[30;107m*\033[0m")
     print("There are [{}] rooms in the world".format(len(rooms)))
     for r in rooms:
-        print_room(r["id"], rooms)
+        print_room_details(r["id"], rooms)
         print()
     logging.debug("Room test dump finished.")
 
 
+def move_player(player, dir, rooms):
+    return
+
+
+def look_at_room(player, rooms):
+    if player["room"] == NOWHERE:
+        print('Floating in the VOID, just look at all the stars!')
+    elif player["room"] < 0 or player["room"] > max_room_id:
+        print('Falling !!!!  Somehow you are outside the world, returning you to the start')
+        player[room] = default_start_room
+        look_at_room(player)
+    else:
+        rm = find_room(player["room"], rooms)
+        if len(rm) != 0:
+            # Remember that this appears white in pycharm :(
+            print('{wht}{rname}{norm}'.format(wht=Screen.fg.BLACK, norm=Screen.fg.NRM, rname=rm["name"]))
+            print('{mag}{rdesc}{norm}'.format(mag=Screen.fg.MAGENTA, norm=Screen.fg.NRM, rdesc=rm["desc"]))
+            ex = rm["exits"]
+            if len(ex) == 0:
+                print("{cyn}Exits: None!{norm}")
+            else:
+                print('todo')
+    return
+
+
 def main():
+    player = {"name": "Dan", "room": dev_start_room, "moves": 20, "max_moves": 20, "health": 10, "max_health": 10,
+              "stuff": []}
+
     running = True
     counter = 0
 
     logging.info('Load all rooms')
     all_rooms = make_rooms()
     test_all_rooms(all_rooms)
+    print('\n\n\n\n')
+    while True:
+        new_name = input("What is your name: ")
+        if new_name == '':
+            print('You must have a name, try again')
+        elif new_name.lower() == 'dan':
+            player["name"] = "Dangerous Daniel"
+            player["room"] = dev_start_room
+            break
+        else:
+            player["name"] = new_name
+            player["room"] = default_start_room
+            break
+        # end while True
+    print('Hello {}, welcome to the world'.format(player["name"]))
+    look_at_room(player, all_rooms)
 
     logging.info('Start main loop')
     while running:
@@ -336,6 +411,17 @@ def main():
         if counter == 20:
             logging.info('Game is now quitting')
             running = False
+        cmd = input("Command: ")
+        lcmd = cmd.lower()
+        if (lcmd == 'quit') or (lcmd == 'q'):
+            break
+        elif (lcmd == 'look') or {lcmd == 'l'}:
+            look_at_room(player, all_rooms)
+
+        sleep(1)
+
+    print('Goodbye')
+
 
 if __name__ == "__main__":
     main()
